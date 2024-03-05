b51ee6985f3586d05d75702421a40be27c265468
Fabricio Luis Perrone <fabluisper@engdb.infra>
2024-03-05 13:57:25 -0300

    update-elementos-combos

--- cypress/e2e/elements/elements.cy.js
+++ cypress/e2e/elements/elements.cy.js
@@ -14,7 +14,7 @@ describe('Work with basci elements', () => {
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
     })
     it('Links', () => {
-        cy.get('[href="#"]', {timeout:30000}).click()
+        cy.get('[href="#"]', { timeout: 30000 }).click()
         cy.get('#resultado').should('have.text', 'Voltou!')
 
         cy.reload()
@@ -71,9 +71,32 @@ describe('Work with basci elements', () => {
         cy.get('[data-test="dataEscolaridade"]')
             .select('1graucomp')
             .should('have.value', '1graucomp')
+
+        cy.get('[data-test=dataEscolaridade] option')
+            .should('have.length', 8)
+        cy.get('[data-test=dataEscolaridade] option')
+            .then($arr => {
+                const values = []
+                $arr.each(function () {
+                    values.push(this.innerHTML)
+                })
+                expect(values).to.include.members(["Superior", "Mestrado"])
+            })
     })
-    it('Combo multiplo', () => {
+    it.only('Combo multiplo', () => {
         cy.get('[data-testid=dataEsportes]')
             .select(['natacao', 'Corrida', 'nada'])
+
+        // cy.get('[data-testid=dataEsportes]')
+        //     .should('have.value', ['natacao', 'Corrida', 'nada'])
+        cy.get('[data-testid=dataEsportes]')
+            .then($el => {
+                expect($el.val()).to.be.deep.equal(['natacao', 'Corrida', 'nada'])
+                expect($el.val()).to.have.length(3)
+            })
+        cy.get('[data-testid=dataEsportes]')
+            .invoke('val')
+            .should('eql', ['natacao', 'Corrida', 'nada'])
+            // TODO: validar opções selecionadas do comnbo multiplo
     })
 })
