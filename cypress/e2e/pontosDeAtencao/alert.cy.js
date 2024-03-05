ec1c9ea495d8feae41760c39655be33f0de45307
Fabricio Luis Perrone <fabluisper@engdb.infra>
2024-03-05 16:23:13 -0300

    alert-confirm

--- cypress/e2e/pontosDeAtencao/alert.cy.js
+++ cypress/e2e/pontosDeAtencao/alert.cy.js
@@ -15,11 +15,30 @@ describe('Work with alerts', () => {
             expect(msg).to.be.equal('Alert Simples')
         })
     })
-    it.only('Alert com mock', () => {
+    it('Alert com mock', () => {
         const stub = cy.stub().as('alerta')// dando um nome -Alias
         cy.on('window:alert', stub)
         cy.get('#alert').click().then(() => {
             expect(stub.getCall(0)).to.be.calledWith('Alert Simples')
         })
+    })
+    it('Confirm', () => {
+        cy.on('window:confirm', msg => {//on() pega eventos que ocorrem na nossa tela
+            expect(msg).to.be.equal('Confirm Simples')
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Confirmado')
+        })
+        cy.get('#confirm').click()
+    })
+    it.only('Deny', () => {
+        cy.on('window:confirm', msg => {//on() pega eventos que ocorrem na nossa tela
+            expect(msg).to.be.equal('Confirm Simples')
+            return false
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Negado')
+        })
+        cy.get('#confirm').click()
     })
 })
