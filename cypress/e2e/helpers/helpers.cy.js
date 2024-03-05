9dee5530aee09499c5756b78ecaba3390abcf6ad
Fabricio Luis Perrone <fabluisper@engdb.infra>
2024-03-05 13:33:10 -0300

    uso do invoke

--- cypress/e2e/helpers/helpers.cy.js
+++ cypress/e2e/helpers/helpers.cy.js
@@ -2,13 +2,13 @@
 
 describe('Helpers...', () => {
     it('Wrap', () => {
-        const obj = {nome: 'User', idade: '20'}
+        const obj = { nome: 'User', idade: '20' }
         expect(obj).to.have.property('nome')
         cy.wrap(obj).should('have.property', 'nome')
 
         cy.visit('https://wcaquino.me/cypress/componentes.html')
-        cy.get('#formNome').type('funciona?', { delay:100})
-        .clear()
+        cy.get('#formNome').type('funciona?', { delay: 100 })
+            .clear()
         cy.get('#formNome').then($el => {
             cy.wrap($el).type('funciona via cypress')
         })
@@ -27,17 +27,34 @@ describe('Helpers...', () => {
         //     return 2
         // }).should('be.equal', '2')
     })
-    it.only('Its...', () => {
-        const obj1 = {nome: 'User', idade: '20'}
+    it('Its...', () => {
+        const obj1 = { nome: 'User', idade: '20' }
         cy.wrap(obj1).should('have.property', 'nome', 'User')
         cy.wrap(obj1).its('nome').should('be.equal', 'User')
 
-        const obj2 = { nome: 'User', idade: 20, endereco: { rua: 'dos bobos'}}
+        const obj2 = { nome: 'User', idade: 20, endereco: { rua: 'dos bobos' } }
         cy.wrap(obj2).its('endereco').should('have.property', 'rua')
         cy.wrap(obj2).its('endereco').its('rua').should('contain', 'bobos')
         cy.wrap(obj2).its('endereco.rua').should('contain', 'bobos')
 
         cy.visit('https://wcaquino.me/cypress/componentes.html')
         cy.title().its('length').should('be.equal', 20)
+    })
+    it.only('Invoke...', () => {
+        const getValue = () => 1
+        const soma = (a, b) => a + b;
+
+        cy.wrap({ fn: getValue }).invoke('fn')
+            .should('be.equal', 1)
+
+        cy.wrap({ fn: soma }).invoke('fn', 2, 5)
+            .should('be.equal', 7)
+
+
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.get('#formNome').invoke('val', 'Texto via invoke')
+        cy.window().invoke('alert', 'Dá pra ver?')// esse método vai me dar o obj win, ou seja, esse obj tem em toda a página e nos da controle sobre toda a tela.
+        cy.get('#resultado')
+            .invoke('html', '<input type="button" value="hached!"/>')
     })
 })
