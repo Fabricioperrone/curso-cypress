73464824819e06416649d6bc4b9ce6e79061ae5f
Fabricio Luis Perrone <fabluisper@engdb.infra>
2024-03-12 14:36:20 -0300

    comandos-de-conta

--- cypress/e2e/barriga/funcional.cy.js
+++ cypress/e2e/barriga/funcional.cy.js
@@ -1,29 +1,20 @@
 /// <reference types="cypress"/>
 
 import loc from  '../../support/locators'
+import '../../support/commandsContas'
 
 describe('Should test at a funcional level', () => {
     beforeEach(() => {
         cy.login('fabricio_luis@outlook.com', 'jaguatirica0901')
         cy.resetApp()
-
-        // cy.visit('https://barrigareact.wcaquino.me/')
-        // cy.get(loc.LOGIN.USER).type('fabricio_luis@outlook.com')
-        // cy.get(loc.LOGIN.PASSWORD).type('jaguatirica0901')
-        // cy.get(loc.LOGIN.BTN_LOGIN).click()
-        // cy.get(loc.MESSAGE).should('contain', 'Bem vindo')
-      
     })
     it('Should create an account', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
-        cy.get(loc.CONTAS.NOME).type('Conta de teste')
-        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.acessarMenuConta()
+        cy.inserirConta('Conta de teste')
         cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso!')
     })
     it('Should update account', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
+        cy.acessarMenuConta()
         cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
         cy.get(loc.CONTAS.NOME)
             .clear()
